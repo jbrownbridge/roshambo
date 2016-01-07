9af02df9fcadd73c2aac13aa6163810b159a3d42
Jason Brownbridge <jbrownbridge@gmail.com>
2016-01-07 10:33:01 +0200

    Port should come from env variable.

--- server/conf.js
+++ server/conf.js
@@ -2,5 +2,5 @@ exports.production = true;
 exports.project_root = "./";
 exports.template_dir = exports.project_root + "templates/";
 exports.static_dir = exports.project_root + "static/";
-exports.port = 8888
+exports.port = process.env.PORT || 8888
 // vi: set et sta sw=2 ts=2:
